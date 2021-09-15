3cc9c137256b46b20d6d147278efa06e33421506
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2021-09-14 21:59:08 -0300

    new project TODO

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom';
 import './Global.css';
 import Badge from '../src/components/Badge'
 import 'bootstrap/dist/css/bootstrap.css'
+import BadgeNew from './pages/BadgeNew'
+import Navbar from './components/Navbar';
 // const element =<h1>Rafa</h1>; = const element = React.createElement('h1',{},'Rafa children') el segundo parametro son los props, o properties
 // const element = React.createElement('a',{href: 'google.com'},'Rafa')
 
 
 const container = document.getElementById('app')
 
+// ReactDOM.render(
+// <Badge firstName = "Rafael" 
+//       lastName = "Cabrera"
+//       jobTitle="Frontend Developer"
+//       avatar = "https://s.gravatar.com/avatar/89217f4ba70f71a8eb88ef4c20d85aa8?s=80"
+//       twitter= "RafaC3D"/> , container)
+
 ReactDOM.render(
-<Badge firstName = "Rafael" 
-      lastName = "Cabrera"
-      jobTitle="Frontend Developer"
-      avatar = "https://s.gravatar.com/avatar/89217f4ba70f71a8eb88ef4c20d85aa8?s=80"
-      twitter= "RafaC3D"/> , container)
+  <BadgeNew />, container)
